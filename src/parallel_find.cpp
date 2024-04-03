a5053c5058fee05e4dff559ad4615e059974358c
Aditya Kaushik <adityakaushik314@gmail.com>
2024-04-03 15:03:25 -0400

    changing structure to avoid breaks in openmp blocks

--- src/parallel_find.cpp
+++ src/parallel_find.cpp
@@ -15,45 +15,40 @@ void parallel_find(const std::string& root, const std::string& target) {
     #pragma omp single nowait
     {
         while (directory_queue.size() > 0) {
-            std::string current_directory;
+            std::string current_directory = 0;
             #pragma omp critical(directory_queue)
             {
                 if (directory_queue.size() > 0) {
                     current_directory = directory_queue.front();
                     directory_queue.pop();
-                } else {
-                    continue;
-                }
+                } 
             }
 
-            #pragma omp task firstprivate(current_directory)
-            {
-                DIR *dir = opendir(current_directory.c_str());
-                if (!dir) {
-                    // std::cerr << "Cannot open directory " << current_directory << std::endl;
-                    return;
+            if (current_directory) {
+                #pragma omp task firstprivate(current_directory)
+                {
+                    DIR *dir = opendir(current_directory.c_str());
+                    if (dir) {
+                        struct dirent *entry;
+                        while ((entry = readdir(dir))) {
+                            std::string entry_name = entry->d_name;
+                            bool fake_dir = entry_name == "." || entry_name == "..";
+                            if (!fake_dir) {
+                                std::string full_path = current_directory + "/" + entry_name;
+                                struct stat status;
+                                if (stat(full_path.c_str(), &status) != -1) {
+                                    if (S_ISDIR(status.st_mode)) {
+                                        #pragma omp critical(directory_queue)
+                                        directory_queue.push(full_path);
+                                    } else if (S_ISREG(status.st_mode) && entry_name == target) {
+                                        std::cout << full_path << std::endl;
+                                    }
+                                }
+                            }
+                        }
+                    closedir(dir);
+                    }
                 }
-                struct dirent *entry;
-                while ((entry = readdir(dir))) {
-                    std::string entry_name = entry->d_name;
-                    if (entry_name == "." || entry_name == "..") {
-                        goto ENDWHILE;
-                    }
-                    std::string full_path = current_directory + "/" + entry_name;
-                    struct stat status;
-                    if (stat(full_path.c_str(), &status) == -1) {
-                        // std::cerr << "Could not get status of " << full_path << std::endl;
-                        goto ENDWHILE;
-                    }
-                    if (S_ISDIR(status.st_mode)) {
-                        #pragma omp critical(directory_queue)
-                        directory_queue.push(full_path);
-                    } else if (S_ISREG(status.st_mode) && entry_name == target) {
-                        std::cout << full_path << std::endl;
-                    }
-                    ENDWHILE:
-                }
-                closedir(dir);
             }
         }
     }
