23b81c65e2d0c069c2bb77b11799657dc8beb6cd
Denis Koshelev <truemacintoshchannel@gmail.com>
2024-04-14 12:39:44 -0400

    Rollback to queue version, fix while termination

--- src/parallel_find.cpp
+++ src/parallel_find.cpp
@@ -5,57 +5,82 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <string>
-#include <vector>
 #include <omp.h>
 
-static bool debug_enabled = false;
+#include <omp.h>
+#include <iostream>
+#include <dirent.h>
+#include <queue>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string>
+#include <atomic>
 
-void process_directory(const std::string& current_directory, const std::string& target) {
-    DIR *dir = opendir(current_directory.c_str());
-    if (!dir) {
-        return;
-    }
+void parallel_find(const std::string& root, const std::string& target) {
+    std::queue<std::string> directory_queue;
+    directory_queue.push(root);
+    std::atomic<int> active_tasks(0);
 
-    std::vector<std::string> entries;
-    struct dirent *entry;
-    while ((entry = readdir(dir))) {
-        entries.push_back(entry->d_name);
-    }
-    closedir(dir);
+    omp_lock_t queue_lock;
+    omp_init_lock(&queue_lock);
 
     #pragma omp parallel
+    #pragma omp single nowait
     {
-        #pragma omp single
-        {
-            for (int i = 0; i < entries.size(); ++i) {
-                std::string entry_name = entries[i];
-                if (entry_name == "." || entry_name == "..") {
-                    continue;
-                }
-                std::string full_path = current_directory + "/" + entry_name;
-                struct stat status;
-                if (stat(full_path.c_str(), &status) == -1) {
-                    continue;
-                }
-                if (S_ISDIR(status.st_mode)) {
-                    #pragma omp task
-                    {
-                        int thread_id = omp_get_thread_num();
-                        if (debug_enabled) {
-                            std::cout << "Thread " << thread_id << " processing directory: " << full_path << std::endl;
+        while (!directory_queue.empty() || active_tasks != 0) {
+            std::string current_directory;
+
+            omp_set_lock(&queue_lock);
+            if (!directory_queue.empty()) {
+                current_directory = directory_queue.front();
+                directory_queue.pop();
+                active_tasks++;
+            }
+            omp_unset_lock(&queue_lock);
+
+            if (!current_directory.empty()) {
+                #pragma omp task firstprivate(current_directory) shared(active_tasks)
+                {
+                    // std::cout << "Thread " << omp_get_thread_num() << " processing: " << current_directory << std::endl;
+                    DIR *dir = opendir(current_directory.c_str());
+                    if (dir) {
+                        struct dirent *entry;
+                        while ((entry = readdir(dir))) {
+                            std::string entry_name = entry->d_name;
+                            if (entry_name == "." || entry_name == "..") {
+                                continue;
+                            }
+                            std::string full_path = current_directory + "/" + entry_name;
+                            struct stat status;
+                            if (stat(full_path.c_str(), &status) != -1) {
+                                if (S_ISDIR(status.st_mode)) {
+                                    omp_set_lock(&queue_lock);
+                                    directory_queue.push(full_path);
+                                    omp_unset_lock(&queue_lock);
+                                    // std::cout << "Thread " << omp_get_thread_num() << " enqueued: " << full_path << std::endl;
+                                } else if (S_ISREG(status.st_mode) && entry_name == target) {
+                                    std::cout << "Thread " << omp_get_thread_num() << " found: " << full_path << std::endl;
+                                }
+                            }
                         }
-                        process_directory(full_path, target);
+                        closedir(dir);
+                    } else {
+                        std::cout << "Thread " << omp_get_thread_num() << " could not open directory: " << current_directory << std::endl;
                     }
-                } else if (S_ISREG(status.st_mode) && entry_name == target) {
-                    int thread_id = omp_get_thread_num();
-                    std::cout << "Thread " << thread_id << " found target: " << full_path << std::endl;
+                    active_tasks--;
                 }
             }
         }
     }
+    #pragma omp taskwait
+    omp_destroy_lock(&queue_lock);
 }
 
-void parallel_find(const std::string& root, const std::string& target) {
-    omp_set_nested(1);
-    process_directory(root, target);
-}
+// int main(int argc, char *argv[]) {
+//     if (argc != 3) {
+//         std::cerr << "Usage: " << argv[0] << " <directory> <target>" << std::endl;
+//         return 1;
+//     }
+//     parallel_find(argv[1], argv[2]);
+//     return 0;
+// }
