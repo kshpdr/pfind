08fe34905cdd2243ba580d2cb4b489e167f18151
Ryan Thomas Lynch <ryan.lynch@gatech.edu>
2024-05-02 00:48:32 -0400

    change synthetic data script again to use function and builtin time

--- tests/synthetic/collect-data-synthetic.sh
+++ tests/synthetic/collect-data-synthetic.sh
@@ -1,37 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
 # generate new file name for this run
-filename="synthetic-$(date +%Y%m%d%H%M%S).csv"
+export filename="synthetic-$(date +%Y%m%d%H%M%S).csv"
 touch $filename
 echo "Writing to $filename"
-echo "directory,keyword,find real time,find user time,find sys time,pfind real time,pfind user time,pfind sys time" > $filename
+echo "directory,keyword,find real time,find user time,find sys time,fdfind real time,fdfind user time,fdfind sys time,b_pfind real time,b_pfind user time,b_pfind sys time,pfind real time,pfind user time,pfind sys time,pfind_rec real time,pfind_rec user time,pfind_rec sys time" > $filename
 
-echo -n "test1,file0," >> $filename
-\time -f '%e,%U,%S,' -a -o $filename find test1 -name "file0" > /dev/null 2>&1
-truncate -s-1 $filename
-\time -f '%e,%U,%S,' -a -o $filename /home/ryan/spring24/cse6230/project/pfind/src/pfind test1 "file0" > /dev/null 2>&1
+export TIMEFORMAT=%R,%U,%S,
 
+# order is find, fdfind, b_pfind, pfind, pfind_rec
 
-echo -n "test1,file1," >> $filename
-\time -f '%e,%U,%S,' -a -o $filename find test1 -name "file1" > /dev/null 2>&1
-truncate -s-1 $filename
-\time -f '%e,%U,%S,' -a -o $filename /home/ryan/spring24/cse6230/project/pfind/src/pfind test1 "file1" > /dev/null 2>&1
+test_ryan_func() {
+    echo -n "$directory,$keyword," >> $filename
+    { time find "$directory" -name "$keyword" > /dev/null 2>&1 ; } 2>> $filename
+    truncate -s-1 $filename
+    { time fdfind "$keyword" "$directory" > /dev/null 2>&1 ; } 2>> $filename
+    truncate -s-1 $filename
+    { time /home/ryan/spring24/cse6230/project/pfind/src/b_pfind "$directory" "$keyword" > /dev/null 2>&1 ; } 2>> $filename
+    truncate -s-1 $filename
+    { time /home/ryan/spring24/cse6230/project/pfind/src/pfind "$directory" "$keyword" > /dev/null 2>&1 ; } 2>> $filename
+    truncate -s-1 $filename
+    { time /home/ryan/spring24/cse6230/project/pfind/src/pfind_rec "$directory" "$keyword" > /dev/null 2>&1 ; } 2>> $filename
+}
 
+export directory="test1"
+export keyword="file0"
+test_ryan_func
 
-echo -n "test1,file5," >> $filename
-\time -f '%e,%U,%S,' -a -o $filename find test1 -name "file5" > /dev/null 2>&1
-truncate -s-1 $filename
-\time -f '%e,%U,%S,' -a -o $filename /home/ryan/spring24/cse6230/project/pfind/src/pfind test1 "file5" > /dev/null 2>&1
+export directory="test1"
+export keyword="file1"
+test_ryan_func
 
+export directory="test1"
+export keyword="file5"
+test_ryan_func
 
+export directory="test2"
+export keyword="file3"
+test_ryan_func
 
-echo -n "test2,file3," >> $filename
-\time -f '%e,%U,%S,' -a -o $filename find test2 -name "file3" > /dev/null 2>&1
-truncate -s-1 $filename
-\time -f '%e,%U,%S,' -a -o $filename /home/ryan/spring24/cse6230/project/pfind/src/pfind test2 "file3" > /dev/null 2>&1
-
-
-echo -n ".,file4," >> $filename
-\time -f '%e,%U,%S,' -a -o $filename find . -name "file4" > /dev/null 2>&1
-truncate -s-1 $filename
-\time -f '%e,%U,%S,' -a -o $filename /home/ryan/spring24/cse6230/project/pfind/src/pfind . "file4" > /dev/null 2>&1
+export directory="."
+export keyword="file4"
+test_ryan_func
