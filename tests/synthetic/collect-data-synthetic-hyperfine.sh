5da7db86c79919d3d9f184f3a6fc6aeb8959f631
Ryan Thomas Lynch <ryan.lynch@gatech.edu>
2024-05-02 06:49:32 -0400

    add script to test synthetic with hyperfine

---
+++ tests/synthetic/collect-data-synthetic-hyperfine.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+export PATH=$PATH:../
+export PATH=$PATH:$(pwd)/../../src/
+
+if ! which hyperfine > /dev/null 2>&1; then
+    echo "'hyperfine' does not seem to be installed."
+    echo "You can get it here: https://github.com/sharkdp/hyperfine"
+    exit 1
+fi
+
+# generate new file name for this run
+export folder="synthetic-hyperfine-$(date +%Y%m%d%H%M%S)-results"
+mkdir -p $folder
+echo "Results in $folder"
+
+# order is find, fdfind, b_pfind, pfind
+
+test_ryan_func() {
+    export filenamecsv="$folder/results-$directory-$keyword.csv"
+    export filenamejson="$folder/results-$directory-$keyword.json"
+    touch $filenamecsv
+    touch $filenamejson
+    hyperfine --sort command -u microsecond -N --export-json "$filenamejson" \
+    --export-csv "$filenamecsv" -r 50 -w 3 \
+        "find $directory -name $keyword  " \
+        "fdfind -uu --glob $keyword $directory  " \
+        "b_pfind $directory $keyword  " \
+        "pfind $directory $keyword  " 
+        # "/home/ryan/spring24/cse6230/project/pfind/src/pfind_rec $directory $keyword  "
+}
+
+export directory="test1"
+export keyword="file0"
+test_ryan_func
+
+export directory="test2"
+export keyword="file3"
+test_ryan_func
+
+export directory="."
+export keyword="file4"
+test_ryan_func
